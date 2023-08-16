ad70994da0b6be910806f27bed8441ed2389bf64
lacey <hunt3247@vandals.uidaho.edu>
2023-08-16 11:46:13 -0700

    wiggle adjusting speed

--- wiggle.py
+++ wiggle.py
@@ -8,44 +8,46 @@ from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT
 import time
 
+speed = 40
+
 def wiggle(mc):
     mc.set_color(0,255,0)
-    mc.send_angles([2.54,107.13,-119.09,-55.28,-9.31,180.0], 50)
-    print('1/3 coords sent')
+    mc.send_angles([2.54,107.13,-119.09,-55.28,-9.31,180.0], speed)
+    #print('1/3 coords sent')
 
     time.sleep(2)
 
-    mc.send_angles([41.57,-2.98,-27.77,-51.94,-66.0,180.0], 50)
-    print('2/3 coords sent')
+    mc.send_angles([41.57,-2.98,-27.77,-51.94,-66.0,180.0], speed)
+    #print('2/3 coords sent')
 
     time.sleep(2)
 
-    mc.send_angles([37.96,-129.19,119.09,46.93,62.57,180.0], 50)
-    print('3/3 coords sent')
+    mc.send_angles([37.96,-129.19,119.09,46.93,62.57,180.0], speed)
+    #print('3/3 coords sent')
 
-    mc.send_angles([0, 0, 0, 0, 0, 0], 50)
-    print('Back to home...')
+    mc.send_angles([0, 0, 0, 0, 0, 0], speed)
+    #print('Back to home...')
 
 def bow(mc):
     mc.set_color(0,0,255)
-    mc.send_angles([53.43,-17.05,112.06,0,0,180.0], 50)
-    print('Taking a left bow...')
+    mc.send_angles([53.43,-17.05,112.06,0,0,180.0], speed)
+    #print('Taking a left bow...')
     time.sleep(3)
 
-    mc.send_angles([50.71,-3.42,2.02,0,0,180.0], 50)
-    print('Coming back up...')
+    mc.send_angles([50.71,-3.42,2.02,0,0,180.0], speed)
+    #print('Coming back up...')
     time.sleep(1)
 
-    mc.send_angles([111.7,0,2.02,0,0,180.0], 50)
-    print('Moving to the right...')
+    mc.send_angles([111.7,0,2.02,0,0,180.0], speed)
+    #print('Moving to the right...')
     time.sleep(1)
 
-    mc.send_angles([111.7,-28.47, 102.12,11.68,0,180.0], 50)
-    print('Taking a right bow...')
+    mc.send_angles([111.7,-28.47, 102.12,11.68,0,180.0], speed)
+    #print('Taking a right bow...')
     time.sleep(3)
 
-    mc.send_angles([0, 0, 0, 0, 0, 0], 50)
-    print('Back to home...')
+    mc.send_angles([0, 0, 0, 0, 0, 0], speed)
+    #print('Back to home...')
 
 
 
@@ -54,12 +56,12 @@ if __name__ == "__main__":
     mc.set_color(255,0,0)
 
     # Zero out position 
-    mc.send_radians([0, 0, 0, 0, 0, 0], 50)
-    print('Finding home first')
+    mc.send_radians([0, 0, 0, 0, 0, 0], speed)
+    #print('Finding home first')
     
     time.sleep(2)
 
     wiggle(mc)
     bow(mc)
     mc.set_color(255,0,0)
-    print('Done')
+    #print('Done')
